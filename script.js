50ee6ddf47f70f67aff8f2010f0e98cde56b4e69
Muhammadali <mukhammadali809@gmail.com>
2023-02-27 23:50:21 +0500

    java scriptda sonlarni juft va toqqa ajratish

---
+++ script.js
@@ -0,0 +1,32 @@
+// let son = prompt("son kiriting")
+// let yosh = prompt("yoshingizni kiriting")
+// let now = prompt("hozirgi yilni kiriting")
+
+// console.log(now);
+// console.log(yosh);
+// console.log(yosh);
+
+
+// let son1 = prompt("1-sonni kiriting")
+// let son2 = prompt("2-sonni kiriting")
+// let son3 = prompt("3-sonni kiriting")
+
+
+
+// if (son1 < son2 && son1 > son3 || son1 > son2 && son1 < son3) {
+//     alert(son1)
+// }else if (son2 < son1 && son2 > son3 || son2 > son1 && son2 < son3) {
+//     alert(son2)
+// }else if (son3 < son1 && son3 > son2 || son3 > son1 && son3 < son2) {
+//     alert(son3)
+// }
+
+let son1 = prompt("son kiriting")
+if (son1 % 2 === 0) {
+    alert("bu just son")
+}else{
+    alert("bu toq son")    
+}
+
+
+
